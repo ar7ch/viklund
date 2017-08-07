0133a00e31db08dd0d70deb0077e46d51897e0d1
Artyom Bulgakov <artyombulgakov@gmail.com>
2017-08-07 19:34:22 +0300

    moved handle_messages() back to main()

--- Vk_messages.py
+++ Vk_messages.py
@@ -15,27 +15,7 @@ class Vk_messages:
 		user = None
 		viklund.Vk_messages.send_selective(item, 'msg', username + ': ' + str(item[u'user_id']))
 	@staticmethod
-	def handle_messages():
-		values = {'out': 0,'count': 100,'time_offset': 60}
-		while True:
-			response = viklund.vk.method('messages.get', values)
-			if response['items']:
-				values['last_message_id'] = response['items'][0]['id']
-			for item in response['items']:
-				recieved_str = item[u'body'].lower()
-				viklund.Vk_system.print_log(item, recieved_str, None)
-				if recieved_str.find(u'лит') != -1 and recieved_str.find(u'рандом') != -1:
-					viklund.Vk_random.handle_random(item, recieved_str)
-				elif recieved_str.find(u'лит') != -1 and recieved_str.find(u'пост') != -1:
-					viklund.Vk_group_import.handle_import_request(item, recieved_str)
-				elif recieved_str.find(u'лит') != -1 and recieved_str.find(u'перешли') != -1:
-					viklund.Vk_messages.resend_user_message(item, recieved_str)
-				elif recieved_str.find(u'лит') != -1 and recieved_str.find(u'айди') != -1:
-					viklund.Vk_messages.handle_id_request(item)
-			recieved_str = u''
-			time.sleep(1)
-	@staticmethod
-	def resend_user_message(item, recieved_str): #TODO: send multiply pictures in one message
+	def resend_user_message(item, recieved_str): #TODO: send multiply pictures in one message, resend via forwarded messages
 		try:
 			for picture in item['attachments']:
 				access_key = ''
