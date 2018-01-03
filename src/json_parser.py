633234767cc43bc0d7c4697b6dc35e7258805033
Artyom Bulgakov <artyombulgakov@gmail.com>
2018-01-03 11:14:22 +0300

    add json example file; add JsonParser class

---
+++ src/json_parser.py
@@ -0,0 +1,63 @@
+#/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+"""
+Copyright (C) 2017-2018 Artyom Bulgakov
+
+This file is part of viklund.
+Viklund is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Viklund is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with viklund.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+import viklund
+
+class JsonParser():
+	@staticmethod
+	def parse_import(json_data):
+		"""
+		Parse import config and return it as dictionary (see example.json)
+		
+		Parameters
+		----------
+		json_data
+			Opened .json file object.
+		Returns
+		_______
+			commands : dict
+				Dictionary with call_command as key and import_id as value.
+		"""
+		commands = {}
+		for items in json_data[section]:
+			commands[items['call_command']] = items['import_id']
+		return commands
+
+	@staticmethod
+	def read_json(path_to_file):
+		"""
+		Read .json import config file.
+
+		Parameters
+		----------
+		path_to_file : string
+			Path to file.
+
+		Returns
+			json_data
+				Opened .json import config file object.
+		_______
+			commands : dict
+				Dictionary with call_command as key and import_id as value.
+		"""
+		with open(path_to_file) as json_file:
+			json_data = json.load(json_file)
+		return json_data
