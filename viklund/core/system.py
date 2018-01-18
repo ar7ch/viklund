54339def8439b5b675fec04c221427d5fc36a8ad
Artyom Bulgakov <artyombulgakov@gmail.com>
2018-01-18 21:42:55 +0300

    add warning message for logging on non-posix systems

--- viklund/core/system.py
+++ viklund/core/system.py
@@ -58,7 +58,9 @@ class System():
 			Viklund будет работать, пока вы не закроете окно, в котором он запущен. 
 			Пожалуйста, рассмотрите возможность использования POSIX-совместимой ОС или WSL (если на Windows)'''
 			viklund.Logging.write_log(viklund.Logging.warning(not_posix_message))
-			
+			if viklund.LOGS_PATH:
+				logs_ignored_message = '''Не могу писать логи в файл на не-POSIX системе, --logs_path аргумент проигнорирован'''
+				viklund.Logging.write_log(viklund.Logging.warning(logs_ignored_message))
 		viklund.vkApi = viklund.vk_session.get_api()
 		success_message = viklund.Logging.success('Бот запущен с PID ' + str(os.getpid()))
 		viklund.Logging.write_log(success_message) #output to terminal
